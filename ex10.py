bafebebdfce25841fad421a8d467ce5360aacfa5
Tiffany Santos <47302131+TiffanySantos@users.noreply.github.com>
2019-07-01 19:07:48 +0200

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,18 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+vertical_cat =  "\v I'm a vertical cat." #appears not to do much... not sure about this one.
+
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(vertical_cat)
---
+++ ex9.py
@@ -0,0 +1,13 @@
+# here is some new strange stiff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:  ", days)
+print("Here are the months:  ", months)
+
+print("""
+There'a something going on here.
+With the three double-quotes.
+Even 4 lines if we want, or 5 or 6.
+""")
