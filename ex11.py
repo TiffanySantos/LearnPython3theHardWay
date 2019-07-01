60187a7e93811363559aef7c84842daad7e788ae
Tiffany Santos <47302131+TiffanySantos@users.noreply.github.com>
2019-07-01 19:21:42 +0200

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,17 @@
+print ("How old are you?", end=' ')
+age = input ()
+print ("How tall are you?", end=' ')
+height = input ()
+print ("How much do you weigh?", end=' ')
+weight = input ()
+
+print (f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print ("Where were you born?", end= ' ')
+birthPlace = input()
+print ("What year were you born?", end=' ')
+birthYear = input()
+print ("Where do you live now?", end= ' ')
+placeLive = input ()
+
+print (f"So you were born in {birthPlace} in {birthYear}, now you live in {placeLive}!")
